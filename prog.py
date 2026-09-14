527adc8fbafd7d42ab5c0faf8bf69cea797ed5f7
Prachi <324512355+PrachiDiwakar24@users.noreply.github.com>
2026-09-14 14:13:05 +0530

    Added electricity bill calculator program

--- prog.py
+++ prog.py
@@ -296,3 +296,20 @@ print("Name:", name)
 print("Total Marks:", total)
 print("Percentage:", percentage, "%")
 print("Grade:", grade)
+
+
+
+# Electricity Bill Calculator
+
+units = float(input("Enter electricity units consumed: "))
+
+if units <= 100:
+    bill = units * 5
+
+elif units <= 200:
+    bill = (100 * 5) + ((units - 100) * 7)
+    
+else:
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+
+print("Electricity Bill =", bill)
